1bd7650a9f18d948e4292a42f67ab8ab307efa79
riemannroch <daniel.alves@morpheustecnologia.com>
2022-11-16 18:12:45 -0300

    Chapter 1, Section 4, Exercise 31

---
+++ src/Chapter1/Section4/Exercise31/PrimeSieve.java
@@ -0,0 +1,27 @@
+package Chapter1.Section4.Exercise31;
+
+
+public class PrimeSieve {
+
+    public static void main( String[] args ) {
+
+        int n = Integer.parseInt( args[ 0 ] );
+        boolean[] isPrime = new boolean[ n + 1 ];
+        for ( int i = 2; i <= n; i++ )
+            isPrime[ i ] = true;
+
+        for ( int i = 2; i <= n / i; i++ ) {
+            if ( isPrime[ i ] ) { // Mark multiples of i as nonprime.
+                for ( int j = i; j <= n / i; j++ )
+                    isPrime[ i * j ] = false;
+            }
+        }
+        // Count the primes.
+        int primes = 0;
+        for ( int i = 2; i <= n; i++ )
+            if ( isPrime[ i ] )
+                primes++;
+        System.out.println( primes );
+    }
+
+}
